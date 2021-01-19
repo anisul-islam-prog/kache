e819a33256134cdeb5e166fcd1cfb3ee5ffa9f83
anisul-islam-prog <kingfahad0802@gmail.com>
2021-01-19 15:21:07 +0600

    Added facebook pixel

--- resources/views/includes/welcome_head.blade.php
+++ resources/views/includes/welcome_head.blade.php
@@ -8,7 +8,7 @@
 <!-- Scripts -->
 
 <title>কাছে</title>
-<meta name="description" content="আপনার আশেপাশের বন্ধু-বান্ধব, প্রতিবেশী কিংবা পরিচিত জনদের সাথে সংযুক্ত থাকুন।"/>
+<meta name="description" content="আপনার আশেপাশের বন্ধু-বান্ধব, প্রতিবেশী কিংবা পরিচিত জনদের সাথে সংযুক্ত থাকুন।" />
 <link rel="shortcut icon" href="{{ URL::asset('favicon.ico') }}" type="image/x-icon" />
 
 
@@ -26,8 +26,37 @@
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-88D188ZQ7Q"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
   gtag('js', new Date());
 
   gtag('config', 'G-88D188ZQ7Q');
 </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<!-- Facebook Pixel Code -->
+<script>
+  ! function(f, b, e, v, n, t, s) {
+    if (f.fbq) return;
+    n = f.fbq = function() {
+      n.callMethod ?
+        n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s)
+  }(window, document, 'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '2854360541475655');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2854360541475655&ev=PageView&noscript=1" /></noscript>
+<!-- End Facebook Pixel Code -->
